d534a4bf31cedafd9b6e302c98fa3930833a4358
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-25 15:33:57 +0200

    add complete ex without bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,29 @@ BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 
 //stampo in console i numeri da 1 a 100
-for (let i = 1; i <= 100; i++){
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+    
+    //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(('FizzBuzz'));
+        
+        //per i multipli di 3 stampo “Fizz” al posto del numero
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+        
+        //per i multipli di 5 stampo “Buzz”
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+        
+    } else {
+        console.log(i);
+        
+    }
+    
+
 }
 
-//per i multipli di 3 stampo “Fizz” al posto del numero
 
-//per i multipli di 5 stampo “Buzz”
 
-//per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+
+
