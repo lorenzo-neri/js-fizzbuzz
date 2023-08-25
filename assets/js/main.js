f0b69d7d1b3f6bfd3d93fb75752c1a37e9f9ec91
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-25 16:46:42 +0200

    add liEl with style

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,33 +10,59 @@ Come faccio a sapere se un numero è divisibile per un altro?
 Consigli del giorno:
 Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
 
-BONUS 1:
-Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 
-BONUS 2:
-Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+//BONUS 1:
+//Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+const containerEl = document.createElement('div');
+const bodyEl = document.querySelector('body');
+const titleEl = document.createElement('h1');
+const ulEl = document.createElement('ul');
+const liEL = document.createElement('li');
+
+bodyEl.append(titleEl);
+bodyEl.append(containerEl);
+titleEl.append('FizzBuzzDOM');
+containerEl.append(ulEl);
+ulEl.append(liEL);
+
+//BONUS 2:
+//Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. 
+
+containerEl.classList.add('container');
+bodyEl.classList.add('text-center', 'bg-dark');
+titleEl.classList.add('text-light');
+
 
 //stampo in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    
+
     //per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log(('FizzBuzz'));
-        
+        console.log('FizzBuzz');
+        liEL.append('FizzBuzz');
+        liEL.classList.add('text-bg-danger');
+
         //per i multipli di 3 stampo “Fizz” al posto del numero
     } else if (i % 3 === 0) {
         console.log('Fizz');
+        liEL.append('Fizz');
+        liEL.classList.add('text-bg-success');
         
         //per i multipli di 5 stampo “Buzz”
     } else if (i % 5 === 0) {
         console.log('Buzz');
-        
+        liEL.append('Buzz');
+        liEL.classList.add('text-bg-warning');
+
     } else {
         console.log(i);
-        
+        liEL.append(i);
+        liEL.classList.add('text-bg-info');
+
     }
-    
 
 }
 
